7677494cb8c44229137aa4478df1b0de9116b89b
flols <fflols22@gmail.com>
2024-04-08 14:40:28 +0200

    feat(Pages): display movies and details movies with id

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,54 @@
-<script></script>
-
 <template>
   <div>
-    <h1 class="text-2xl">Movies</h1>
+    <SearchBar @search-movie="searchMovie"
+    :searchError="searchError" />
+    <div
+      class="grid md:grid-cols-4 sm:grid-cols-1 justify-items-center bg-cyan-50"
+    >
+    <div v-for="n in 8">
+      <MovieCard 
+      :title="title" 
+      :date="date" 
+      :poster="poster" 
+      :movieid="movieid"
+    />
+    </div>
+  </div>
+
+    <!-- <h1>Films Populaires</h1>
+    <div v-if="pending">Chargement...</div>
+    <div v-if="error">Une erreur est survenue.</div>
+    <ul v-else>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie.title }}
+      </li>
+    </ul> -->
   </div>
 </template>
+
+<script setup>
+import { useAsyncData } from 'nuxt/app';
+import { fetchMovies } from '~/services/themoviedb';
+
+const { data: movies, pending, error } = useAsyncData('movies', fetchMovies);
+
+//Search Movie
+const searchError = ref(false);
+const searchMovie = (searchInput) => {
+  // console.log("Search Text: ", searchInput);
+  if (searchInput === "") {
+    searchError.value = true;
+  }
+  if (searchInput !== "") {
+    searchError.value = false;
+  }
+};
+
+// Fetch Movie
+const movieid = ref(123);
+const title = ref("Dune 2");
+const date = ref("01-01-2000");
+const poster = ref("https://www.themoviedb.org/t/p/w1280/iRNbRAIGQQr5diGnjpwJFm0dgt4.jpg");
+
+
+</script>
---
+++ pages/movie/[id].vue
@@ -0,0 +1,32 @@
+<script setup>
+definePageMeta({
+    layout:"custom",
+});
+// Fetch Movie
+const title = ref("Dune 2");
+const date = ref("01-01-2000");
+const poster = ref("https://www.themoviedb.org/t/p/w1280/iRNbRAIGQQr5diGnjpwJFm0dgt4.jpg");
+const revenue = ref(654654654);
+const overview = ref(
+    "Le voyage mythique de Paul Atreides qui s'allie à Chani et aux Fremen dans sa quête de vengeance envers les conspirateurs qui ont anéanti sa famille. Devant choisir entre l'amour de sa vie et le destin de l'univers, il fera tout pour éviter un terrible futur que lui seul peut prédire."
+    );
+
+</script>
+
+
+<template>
+    <div>
+        <MovieDetail
+            :title="title"
+            :date="date"
+            :poster="poster"
+            :revenue="revenue"
+            :overview="overview"
+        />
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
